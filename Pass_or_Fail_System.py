3285716e7030469f1acba1d2b58a1c7203c39a1b
Piyush Drax <ranjan.haz07@gmail.com>
2026-02-10 21:44:26 +0530

    Implement pass/fail system based on marks

--- Pass_or_Fail_System.py
+++ Pass_or_Fail_System.py
@@ -1,1 +1,15 @@
+subject_marks = []
 
+for i in range(3):
+    marks = int(input("Enter your marks:"))
+    subject_marks.append(marks)
+
+total_percentage = sum(subject_marks) / len(subject_marks) # i know i can use 3 here!
+
+if total_percentage >= 40 and min(subject_marks) >= 33:
+  print("You have passed the exam!")
+  print("Percentage:", total_percentage ,"%")
+
+else:
+  print("You have failed!)
+  print("Percentage:", total_percentage, "%")
